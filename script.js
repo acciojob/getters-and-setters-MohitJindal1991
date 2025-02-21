eefe3be8ddc8cf77583c1ea9d090c6ac272c7471
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-21 18:58:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Person {}
+class Person {
+	let name;
+	let age;
 
-class Student extends Person {}
+	getname(name){
+		this.name=name;
+	}
+	setage(age){
+		this.age=age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
